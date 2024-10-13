0bbd551851a05378ba08df6081e62be6fb809484
dianagiangregorio <diana.giangregorio@gmail.com>
2024-10-13 22:19:36 +0200

    feat: added element in page

--- main.js
+++ main.js
@@ -2,17 +2,26 @@
 
 const startValue = 1;
 const endValue = 100;
+const fizzBuzzContainer = document.getElementById('fizzbuzz-container')
+const resultFizzBuzz = document.getElementById('fizzbuzz')
 
 //stampa Fizz, Buzz o FizzBuzz se il numero è multiplo di 3, 5 o entrambi
+
+let result = ''
 
 for (let n = startValue; n <= endValue; n++){
     if ((n % 3 === 0 ) && (n % 5 === 0)){
         console.log('FizzBuzz');
+        result = 'FizzBuzz'
     } else if (n % 5 === 0){
         console.log('Buzz');
+        result = 'Buzz'
     } else if (n % 3 === 0 ) {
         console.log('Fizz')
+        result = 'Fizz'
     } else {
         console.log(n)
+        result = n
     }
 }
+resultFizzBuzz.innerHTML = `${result}`
